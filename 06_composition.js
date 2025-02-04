780be144a845805662680d405cfe14061debcf93
sabrine <labiedhsabrine@gmail.com>
2025-02-04 11:12:49 +0100

    exo6

--- 06_composition.js
+++ 06_composition.js
@@ -8,7 +8,7 @@
  *  - for, foreach, while, do while sont interdits
  */
 
-const concat = (arr1, arr2) => {}
+const concat = (arr1, arr2) =>([...arr1, ...arr2]);
 
 /**
  * utiliser l'opérateur de composition ... afin d'ajouter un élément à un tableau
@@ -19,15 +19,14 @@ const concat = (arr1, arr2) => {}
  * - ne pas utiliser la méthode push
  */
 
-const push = (arr, item) => {}
-
+    const push = (arr, item) =>([...arr, item]);
 /**
  * utiliser l'opérateur de composition ... afin de fusionner 2 objets passés en paramètres
  * 
  * ex: {a: 1, b: 2}, {c: 3, d: 4} => {a: 1, b: 2, c: 3, d: 4}
  */
 
-const merge = (obj1, obj2) => {}
+const merge = (obj1, obj2) => ({...obj1, ...obj2});
 
 /**
  * utiliser l'opérateur de composition ... afin de modifier la propriété name de l'objet
@@ -39,6 +38,8 @@ const merge = (obj1, obj2) => {}
  */
 
 const setName = (obj, name) => {}
-
+console.log(concat([1, 2, 3], [4, 5, 6]));
+console.log(push([1, 2, 3], 4));
+console.log(merge({a: 1, b: 2}, {c: 3, d: 4}));
 // astuce: {...obj} crée une copie de l'objet, c'est un des principes de l'immutabilité et évite les problèmes de référence
 module.exports = {concat, push, merge, setName}
