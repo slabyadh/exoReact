f9f1a7d06b5532f20b5792e3c852849d638faf89
sabrine <labiedhsabrine@gmail.com>
2025-02-04 10:44:31 +0100

    exo5

--- 05_arrayOperations.js
+++ 05_arrayOperations.js
@@ -10,7 +10,8 @@
  * 
   */
 
-const multiplyByTwo = (array) => {}
+const multiplyByTwo = (array) => array.map(x => x * 2);
+
 
 
 /**
@@ -23,7 +24,7 @@ const multiplyByTwo = (array) => {}
  *   - Vous ne pouvez pas utiliser de variable (autre que l'argument de la fonction)
   */
 
-const filterNameStartByA = (array) => {}
+const filterNameStartByA = (array) => array.filter(x => x[0] === 'A');
 
 /**
  * Utiliser la fonction .reduce sur le tableau passé en paramètre
@@ -35,7 +36,7 @@ const filterNameStartByA = (array) => {}
  *   - Vous ne pouvez pas utiliser de variable (autre que l'argument de la fonction)
   */
 
-const sum = (array) => {}
+const sum = (array) => array.reduce((sum, x) => sum + x);
 
 /**
  * Utiliser la fonction .find sur le tableau passé en paramètre
@@ -56,7 +57,11 @@ const sum = (array) => {}
  *   - Vous ne pouvez pas utiliser de variable (autre que l'argument de la fonction)
   */
 
-const findUserById = (array, id) => {}
+const findUserById = (array, id) =>   array.find(x => x.id === id).name;
 
+console.log(multiplyByTwo([1, 2, 3, 4]));
+console.log(filterNameStartByA(['Amanda', 'Walid', 'Alice', 'Guillian']));
+console.log(sum([1, 2, 3, 4]));
+console.log(findUserById([{id: 1, name: 'John'}, {id: 2, name: 'Doe'}, {id: 3, name: 'Foo'}, {id: 4, name: 'Bar'}], 3));
 
 module.exports = {multiplyByTwo, filterNameStartByA, sum, findUserById};
