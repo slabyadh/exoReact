a799b7d4be85e764ad435c94d1df6a4d8a890e5e
sabrine <labiedhsabrine@gmail.com>
2025-02-04 11:30:36 +0100

    exo8

--- 08_chainingOperator.js
+++ 08_chainingOperator.js
@@ -16,6 +16,8 @@
  * 
  */
 
-const getGroupName = (obj) => {}
+const getGroupName = (obj) => (obj.group?.name);
 
+console.log(getGroupName({name: "John", group: {name: "admin"}}));
+console.log(getGroupName({name: "John"})); 
 module.exports = {getGroupName};
